2ef523cdb2a4e735c51186008ceffc403963ba09
CodeChd <Drowzyking1126@gmail.com>
2023-12-16 23:19:46 +0800

    Clean up

--- client/src/components/Hero/Hero.tsx
+++ client/src/components/Hero/Hero.tsx
@@ -5,9 +5,9 @@ function Hero() {
     return (
         <div className="grid grid-cols-2 gap-2 py-4 my-8 items-center">
             <motion.div
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 1.3, type: "spring"}} >
+                initial={{opacity: 0, scale: 0.5}}
+                animate={{opacity: 1, scale: 1}}
+                transition={{duration: 1.3, type: "spring"}}>
                 <h2 className="text-2xl uppercase font-semibold brightness-75">I Build</h2>
                 <p className="text-6xl font-bold mb-8">Applications and share tips and tricks.</p>
                 <p className="text-2xl text-zinc-400 brightness-125 mt-8"> Let's simplify the complex and make it more
